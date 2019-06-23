c4d7d582e7096b3d9e6da2a1ed66355164fe4a8d
Jeremias ramirez <jereramirez499@gmail.com>
2019-06-23 09:40:35 -0400

    expandiendo formulario

--- menuHierachy.py
+++ menuHierachy.py
@@ -1,6 +1,7 @@
+from tkinter import *
+from tkinter import messagebox as MessageBox
 
 maxChange = 0
-
 
 def mostrar(v1, v2):
 	global maxChange
@@ -8,12 +9,28 @@ def mostrar(v1, v2):
 
 	savevalue = v2.get()
 
-	print(savevalue)
-	if maxChange < 2 and v2.get() != "":	
-		v1.set(savevalue)
-		v2.set("Solo puedes editar 1 vez")
-		text2.config(state="disabled")
-		text.config(state="disabled")
+	# print(savevalue)
+	
+	if v2.get() == "" or v2.get() == " ":
+		MessageBox.showerror("Completar", "No puede estar vacio")
+
+	if len(v2.get()) < 3:
+		MessageBox.showerror("Completar", "Debe tener al menos 3 caracteres")
+
+	else:
+		if maxChange < 2:
+			v1.set(savevalue)
+			MessageBox.showerror("Edicion", "Solo se puede editar 1 vez")
+
+		else:
+			v2.set("Solo puedes editar 1 vez")
+			text2.config(state="disabled")
+			text.config(state="disabled")
+
+	
+		
+		
+	
 
 def call():
 	mostrar(val, val2)
@@ -24,13 +41,13 @@ root.resizable(0, 0)
 val = StringVar()
 val2 = StringVar()
 
-img = None
+# img = None
 def create():
 	rootwindow = Tk(className="Nuevo")
 	rootwindow.config(bg="orange", bd=0, width=400, height=680)
 	baseroot = rootwindow.mainloop()
 
-img = PhotoImage(file="icon2.png")
+# img = PhotoImage(file="icon2.png")
 menubar = Menu(root)
 
 menubar.config(font="Arial", fg="white", bg="black")
@@ -44,7 +61,9 @@ menuitem.add_command(label="Cerrar", command=root.quit)
 menubar.add_cascade(label="Menu", menu=menuitem)
 
 root.config(menu=menubar)
+
 val.set("Viejo valor")
+
 text = Entry(root, textvariable=val)
 text.place(x=0, y=0)
 text.config(width=30, bd=0, font="Arial", selectbackground="orange")
@@ -52,11 +71,11 @@ text.config(width=30, bd=0, font="Arial", selectbackground="orange")
 text2 = Entry(root, textvariable=val2)
 text2.place(x=0, y=30)
 text2.config(width=30, bd=0, font="Arial", selectbackground="orange")
+
 val2.set(val.get())
+
 button = Button(root)
 button.place(x=300, y=0)
- 
- 
 button.config(text="actualizar", command=call)
  
 
@@ -68,4 +87,4 @@ button.config(text="actualizar", command=call)
 
 
 
-root.mainloop()
+base = root.mainloop()
